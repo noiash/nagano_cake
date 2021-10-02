cb6d89ee160958dfb7c028e1d903b995e16ac524
EC2 Default User <ec2-user@ip-172-31-94-54.ec2.internal>
2021-10-02 12:52:23 +0000

    [Update]応用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  private
+
+   def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+        admins_orders_top_path
+    else
+        root_path
+    end
+  end
+
 end
