f6a6be728de8bf064e8c286cc97c1a61e040a397
noiash <noinoi51267@gmail.com>
2021-10-03 14:03:27 +0000

    [Update]応用

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :admins
+
+  devise_for :customers
+  devise_for :admins, path: 'admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
-    get '/' => 'homes#top'
-    resources :items
+    root to: 'homes#top'
+    resources :items, except:[:destroy]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:edit, :update]
+    resources :order_details, only:[:update]
   end
 
-  namespace :public do
+  scope module: :public do
     get '/' => 'homes#top'
+    resources :items, only:[:index, :show]
+    resources :customers, only:[:show, :edit, :update]
+    get '/customers/unsubscribe' => 'customers#unsubscribe'
+    patch '/customers/withdraw' => 'customers#withdraw'
+    resources :cart_items, only:[:index, :update, :create, :destroy]
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :orders, only:[:new, :index, :show, :create]
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/complete' => 'orders#complete'
+    resources :addresses, except:[:show, :new]
   end
 
 
