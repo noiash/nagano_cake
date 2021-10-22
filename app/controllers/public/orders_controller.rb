27d4fc502e30fe7ecc217b231a7f26befe3a29e5
noiash <noinoi51267@gmail.com>
2021-10-22 12:00:08 +0000

    [Update]応用

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,26 +1,23 @@
 class Public::OrdersController < ApplicationController
 
   def new
-    @order = Order.new
   end
 
   def confirm
-    @order = Order.new
-    @cart_items = current_customer.cart_items.all
-    if params[:order][:select_method] == 1
-      @order.address = current_customer.address
+    if @order = params[:order][:select_address] == 1
       @order.postal_code = current_customer.postal_code
-      @order.name = current_customer.full_name
-    elsif params[:order][:select_method] == 2
-      @sta = params[:order][:order_address].to_i
-      @order_address = Address.find(@sta)
-      @order.postal_code = @order_address.postal_code
-      @order.order_address = @order_address.address
-      @order.name = @order_address.name
-    elsif params[:order][:select_method] == 3
-      @order.postal_code = params[:order][:postal_code]
-      @order.order_address  = params[:order][:order_address]
-      @order.name = params[:order][:name]
+   　  @order.address = current_customer.address
+　　  @order.name = current_customer.full_name
+
+    elsif @order = params[:order][:select_address] == 2
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+
+    elsif @order = params[:order][:select_address] == 3
+      @order = Order.new(order_params)
+
     end
 
     redirect_to orders_confirm_path
